7e935c918a10552ff7d13d966df8b2c012f09396
Yoann Destras <yoanndestras@gmail.com>
2022-03-18 16:25:19 +0100

    test heroku app

--- app.js
+++ app.js
@@ -114,7 +114,7 @@ app.use(`/v2/orders`, orderRouteV2);
 app.use(`/v2/imageUpload`, uploadRouterV2);
 app.use(`/v2/stripe`, stripeRouteV2);
 
-app.use(express.static(path.join(__dirname, "nutritiv-fe")));
+app.use(express.static(path.join(__dirname, "/nutritiv-fe/build")));
 
 app.get("*", (req, res) =>
 {

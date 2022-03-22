16f3d8ee72cce044d70898b257282b360c3f87ba
Yoann Destras <yoanndestras@gmail.com>
2022-03-22 17:37:21 +0100

    socketio

--- app.js
+++ app.js
@@ -49,7 +49,7 @@ const io = require("socket.io")(http,
     {
         cors: 
         {
-            origin: "http://localhost:4000",
+            origin: "http://localhost:3000",
             methods: ["GET", "POST"],
             credentials: true
         

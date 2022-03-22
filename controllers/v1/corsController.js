801b3638f98d7416c4b5f64db811e12e9ec3dc10
Yoann Destras <yoanndestras@gmail.com>
2022-03-22 18:13:02 +0100

    cors test

--- controllers/v1/corsController.js
+++ controllers/v1/corsController.js
@@ -6,7 +6,7 @@ const corsOptionsDelegate = (req, callback) =>
 {
     let corsOptions;
     
-    if(whitelist.indexOf(req.header('Origin')) !== -1) 
+    if(whitelist.indexOf("https://nutritiv-hy.herokuapp.com/") !== -1) 
         {
             corsOptions = 
             { 

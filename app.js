31b3fe64f5bc42f35f74e5788ca5555c253738e0
Yoann Destras <yoanndestras@gmail.com>
2022-03-24 19:35:23 +0100

    socketio jwt

--- app.js
+++ app.js
@@ -28,10 +28,7 @@ const io = require("socket.io")(http,
 
 io.use((socket, next) => 
 {
-    console.log(`socket.handshake = `, socket.handshake)
-    console.log(`socket.handshake.token = `, socket.handshake.token)
-    console.log(`socket = `, socket)
-    console.log(`socket.token = `, socket.token)
+    console.log(`socket.handshake.refreshToken = `, socket.handshake.token)
     
     if(socket && socket.token)
     {

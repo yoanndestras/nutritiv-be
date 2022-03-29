42da375857cdcd2840947b2772a52a1dce9dde9a
Yoann Destras <yoanndestras@gmail.com>
2022-03-29 14:06:58 +0100

    socketio err handling

--- app.js
+++ app.js
@@ -57,7 +57,8 @@ io.on("connection", (socket) =>
 
     socket.use((socket, next) => 
     {
-        let refreshToken = socket.handshake.query.refreshToken;
+        let refreshToken = socket.handshake?.query?.refreshToken;
+        console.log(socket.handshake?.query?.refreshToken);
         let sender = jwt.verify(refreshToken, process.env.REF_JWT_SEC, (err, decoded) =>
         {
             if(decoded?._id)

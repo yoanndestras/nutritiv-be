0bb704a3bedda9f44624f964cf5aaed94137b054
Yoann Destras <yoanndestras@gmail.com>
2022-03-31 15:21:35 +0100

    heroku commit

--- client/src/Components/Chat.js
+++ client/src/Components/Chat.js
@@ -7,7 +7,7 @@ import { addMessage, getLastMessageOfRoom } from '../Redux/reducers/messages';
 const token = localStorage.getItem('refresh_token');
 // const token = "ThisIsSomeIncorrectToken"
 const socket = io(
-  "http://localhost:4000",
+  process.env.REACT_APP_API_ADDRESS,
   {
     query: { token },
     // transports: ['websocket']

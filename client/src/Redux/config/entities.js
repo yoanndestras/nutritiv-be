e16968fb5b9852db57cd807f78428a571c521c19
Yoann Destras <yoanndestras@gmail.com>
2022-04-04 15:35:00 +0100

    socketIo

--- client/src/Redux/config/entities.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from "redux";
-import userSlice from '../reducers/user';
-import messagesSlice from '../reducers/messages';
-
-export default combineReducers({
-    // products: productsSlice,
-    user: userSlice,
-    messages: messagesSlice,
-})

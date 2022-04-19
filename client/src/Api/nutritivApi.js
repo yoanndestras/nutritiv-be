8fb750c1995b7d457dffc766d0b5241bbe07f509
Yoann Destras <yoanndestras@gmail.com>
2022-04-19 13:56:52 +0100

    new deploy

--- client/src/Api/nutritivApi.js
+++ client/src/Api/nutritivApi.js
@@ -12,10 +12,9 @@ export const injectStore = _store => {
 
 // # API INSTANCE #
 const apiVersion = process.env.REACT_APP_API_VERSION
-const apiAddress = process.env.REACT_APP_API_ADDRESS_FULL
 
 const nutritivApi = axios.create({
-  baseURL: `${apiAddress}/${apiVersion}`,
+  baseURL: `${apiVersion}`,
 })
 
 // # INTERCEPTORS #

59cafe657728722d0e7361c9de008d6d8691fdaf
Yoann Destras <yoanndestras@gmail.com>
2022-05-31 15:04:24 +0100

    test

--- app.js
+++ app.js
@@ -56,10 +56,10 @@ mongoose
         console.log(err);
     });
 
-// const DB_NAME = process.env.DB_NAME;
-// const currentDay = new Date().toLocaleDateString('pt-PT').replace(/\//g,'-');
-// const ARCHIVE_PATH = path.join(__dirname, 'public/dbBackups', `${currentDay}_${DB_NAME}.gzip`);
-// cron.schedule('0 6 * * *', () => backupMongoDB(DB_NAME, ARCHIVE_PATH)); // SAVE A DB BACKUP EVERYDAY AT 5 AM
+const DB_NAME = process.env.DB_NAME;
+const currentDay = new Date().toLocaleDateString('pt-PT').replace(/\//g,'-');
+const ARCHIVE_PATH = path.join(__dirname, 'public/dbBackups', `${currentDay}_${DB_NAME}.gzip`);
+cron.schedule('0 6 * * *', () => backupMongoDB(DB_NAME, ARCHIVE_PATH)); // SAVE A DB BACKUP EVERYDAY AT 5 AM
 
 app.use(express.json()); // APP LEARN TO READ JSON
 app.use(express.urlencoded({extended: true})); // APP LEARN TO READ JSON  
@@ -79,8 +79,8 @@ app.use(
         })
     ) // LIMIT SPAM REQUESTS TO MAX PER MILLISECONDS
 app.use(express.static(path.join(__dirname, 'public'))); // USE STATIC FILES ON PUBLIC FOLDER
-// app.use(express.static(path.join(__dirname, "/client/build"))); // STATIC FILES FOR FRONT-END APP
-// app.get("*", (req, res) =>{res.sendFile(path.join(__dirname, "/client/build", "index.html"))});
+app.use(express.static(path.join(__dirname, "/client/build"))); // STATIC FILES FOR FRONT-END APP
+app.get("*", (req, res) =>{res.sendFile(path.join(__dirname, "/client/build", "index.html"))});
 
 app.use((err, req, res, next) =>
 {

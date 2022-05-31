bc9c12f96715c55b114cb8707e6eb69ba1e20120
Yoann Destras <yoanndestras@gmail.com>
2022-05-31 15:12:05 +0100

    new release

--- client/src/Helpers/localStorage.js
+++
@@ -1,8 +0,0 @@
-export const storageKeys = {
-    accessToken: 'access_token',
-    refreshToken: 'refresh_token',
-}
-export const storageValues = {
-    accessToken: localStorage.getItem(storageKeys.accessToken),
-    refreshToken: localStorage.getItem(storageKeys.refreshToken),
-}

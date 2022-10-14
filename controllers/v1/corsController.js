25af76beb3827e15c54685d67e8b95c8e837b27b
Yoann <yoanndestras@gmail.com>
2022-10-14 17:39:35 +0200

    clg /self error

--- controllers/v1/corsController.js
+++ controllers/v1/corsController.js
@@ -19,7 +19,7 @@ const corsOptionsDelegate = (req, callback) =>
                 origin: true, 
                 credentials: true, 
                 exposedHeaders: ['access_token', 'refresh_token', 'twofa_token', 'new_twofa_token'], 
-                allowedHeaders: ['X-Requested-With', 'X-HTTP-Method-Override', 'Content-Type', 'Accept', 'Content-Length', 'X-Foo', 'X-Bar', 'access_token', 'refresh_token', 'twofa_token', 'new_twofa_token']
+                allowedHeaders: ['X-Forwarded-Host', 'X-Requested-With', 'X-HTTP-Method-Override', 'Content-Type', 'Accept', 'Content-Length', 'X-Foo', 'X-Bar', 'access_token', 'refresh_token', 'twofa_token', 'new_twofa_token']
             };
             console.log({ origin: true });
         }
